b095e6861e947ce559db36e32af92791e31d9962
Toby McLaughlin <toby@jarpy.net>
2016-11-03 13:22:15 +1100

    Include lib64 in deployment package.

--- lambkin/zip.py
+++ lambkin/zip.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 
 import os
+import re
 import zipfile
 import lambkin.metadata as metadata
 
@@ -13,20 +14,16 @@ def create_zip():
     zip_file = zipfile.ZipFile(zip_file_path, 'w', zipfile.ZIP_DEFLATED)
 
     for root, dirs, files in os.walk('.'):
-        site_dir = os.path.join('.', 'venv', 'lib', 'python2.7', 'site-packages')
-        dist_dir = os.path.join('.', 'venv', 'lib', 'python2.7', 'dist-packages')
         for f in files:
             path = os.path.join(root, f)
+            regex = '\./venv/lib(?:64)?/python\d\.\d/(?:site|dist)-packages/(.*)'
+            package_match = re.match(regex, path)
             if path.endswith('.pyc'):
                 pass
-            elif root.startswith(site_dir):
+            elif package_match:
                 # Then strip the library dir, and put the file in the zip.
-                trimmed_path = path[len(site_dir):]
-                zip_file.write(path, trimmed_path)
-            elif root.startswith(dist_dir):
-                # Then strip the library dir, and put the file in the zip.
-                trimmed_path = path[len(dist_dir):]
-                zip_file.write(path, trimmed_path)
+                trimmed_library_file = package_match.group(1)
+                zip_file.write(path, trimmed_library_file)
             elif root.startswith('./venv') or root.startswith('./.git'):
                 # Then it's other junk that we don't want.
                 pass
